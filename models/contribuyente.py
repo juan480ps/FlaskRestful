53f15c3ebaded2dcc8de60b2b9d08a5345b0bed1
juan480ps <you@example.comjcorrea480@outlook.com>
2023-05-05 15:30:16 -0400

    a

--- models/contribuyente.py
+++ models/contribuyente.py
@@ -1,6 +1,6 @@
 from db import db
 
-class ContribuyenteModel(db.Model):   
+class ContribuyenteModel():   
     __tablename__ = 'contribuyente'
 
     id = db.Column(db.Integer, primary_key=True)
@@ -10,36 +10,38 @@ class ContribuyenteModel(db.Model):
     mescierre = db.Column(db.String(256), nullable=False)
     razonsocial = db.Column(db.String(256), nullable=False)
     ruc = db.Column(db.String(256), nullable=False)    
+    operacion = ""
 
     def __str__(self):
-        return self.razonsocial
+        return self.id
 
-    def __init__(self, categoria, dv, estado, mescierre, razonsocial, ruc, operacion):
+    def __init__(self, operacion, id, ruc, categoria, dv, estado, mescierre, razonsocial):
         self.categoria = categoria
         self.dv = dv
         self.estado = estado
         self.mescierre = mescierre
         self.razonsocial = razonsocial
         self.ruc = ruc
+        self.id = id
         self.operacion = operacion
 
-    def json(self):
-        return {
-            'categoria' : self.categoria, 
-            'dv' : self.dv, 
-            'estado' : self.estado, 
-            'mescierre' : self.mescierre, 
-            'razonsocial' : self.razonsocial, 
-            'ruc' : self.ruc
-        }
+    # def json(self):
+    #     return {
+    #         'categoria' : self.categoria, 
+    #         'dv' : self.dv, 
+    #         'estado' : self.estado, 
+    #         'mescierre' : self.mescierre, 
+    #         'razonsocial' : self.razonsocial, 
+    #         'ruc' : self.ruc
+    #     }
     
-    @classmethod
-    def find_by_name(cls, name):
-        return cls.query.filter_by(razonsocial=name).first()
+    # @classmethod
+    # def find_by_name(cls, name):
+    #     return cls.query.filter_by(razonsocial=name).first()
     
-    def save_to_db(self):
-        db.session.add(self)
-        db.session.commit()
+    # def save_to_db(self):
+    #     db.session.add(self)
+    #     db.session.commit()
 
     
 
